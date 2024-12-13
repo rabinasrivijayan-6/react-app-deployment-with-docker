9be2985a0fa1cbc560369e05a548f2b70c33048c
rabinasrivijayan-6 <robiquejayni@gmail.com>
2024-12-13 17:51:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ docker rm react
 docker build -t react-ci/cd .
 
 #running a container from the created image:
-docker run -d -it --name react -p 80:80 react-ci/cd
+docker run -d  react-ci/cd
 
 #pushing the image to dockerhub:
 docker tag react-ci/cd ravivarman46/react-app:ci-cd
